b5d0f353f2b03b873e1023df4924d3d3003d8871
bilal-ibrahim <bilalibrahim.listen@gmail.com>
2025-02-21 17:41:04 +0300

    row

--- netflix-app/src/utils/requests.js
+++ netflix-app/src/utils/requests.js
@@ -1,14 +1,13 @@
 const API_KEY = process.env.REACT_APP_APIKEY;
-console.log("API Key:", API_KEY);
 const requests={
-    fetchTreding:`treding/all/week?api_key=${API_KEY}&language=en-us`,
+    fetchTrending:`trending/all/week?api_key=${API_KEY}&language=en-us`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-    fetchTopRatedMovies:`/discover/top_rated?api_key=${API_KEY}&anguage=en-us`,
+    fetchTopRatedMovies:`/movie/top_rated?api_key=${API_KEY}&anguage=en-us`,
     fetchActionMovies:`/discover/movie?api_key=${API_KEY}&with_genres=28`,
     fetchComedyMovies:`/discover/movie?api_key=${API_KEY}&with_genres=35`,
     fetchHorrorMovies:`/discover/movie?api_key=${API_KEY}&with_genres=27`,
     fetchRomanceMovies:`/discover/movie?api_key=${API_KEY}&with_genres=10749`,
     fetchDocumentaries:`/discover/movie?api_key=${API_KEY}&with_genres=99`,
-    fetchTvShows:`top/popular/?api_key=${API_KEY}&anguage=en-us&page=1`,
+    fetchTvShow:`movie/popular?api_key=${API_KEY}&language=en-US&page=1`,
 }
 export default requests;
